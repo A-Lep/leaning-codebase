6f4d8c763d5314015ebed69046c41e88e99e09a3
Alex Lepore <38599172+A-Lep@users.noreply.github.com>
2020-01-17 19:48:26 -0500

    Delete lesson 2 if(2).py

--- Lesson 1/lesson 2 if(2).py
+++
@@ -1,9 +0,0 @@
-in1=input('Please type in your first argument')
-in2=input('Please type in your second argument')
-
-if int(in1)==int(in2):
-    print ('They are the same')
-if int(in1)>int(in2):
-    print ('The first ones greater')
-if int(in1)<int(in2):
-    print ('The second ones greater')
